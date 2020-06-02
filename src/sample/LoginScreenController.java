036299d02a2bd341fdac4096cc59c4e407ee5598
Andrzejasty <aszpit@gmail.com>
2020-06-02 17:58:56 +0200

    Initial commit

---
+++ src/sample/LoginScreenController.java
@@ -0,0 +1,55 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class LoginScreenController {
+    private MainScreenController mainScreenController;
+
+    public void setMainScreenController(MainScreenController mainScreenController) {
+        this.mainScreenController = mainScreenController;
+    }
+
+    @FXML
+    private TextField loginTextArea;
+
+    @FXML
+    private TextField passwordTextArea;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button createNewAccountButton;
+
+    public void setErrorsLable(Label errorsLable) {
+        this.errorsLable = errorsLable;
+    }
+
+    @FXML
+    private Label errorsLable;
+
+    public void createNewAccountOnAction() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("createNewAccount.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        CreateNewAccountController createNewAccountController = loader.getController();
+        createNewAccountController.setMainScreenController(mainScreenController);
+        mainScreenController.setScreen(pane);
+    }
+
+    public void loginButtonOnAction(){
+
+    }
+}
---
+++ src/sample/MainScreenController.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class MainScreenController {
+    @FXML
+    private StackPane mainStackPane;
+
+    @FXML
+    public void initialize(){
+        loadLoginScreen();
+    }
+
+    @FXML
+    public void loadLoginScreen() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("loginScreen.fxml"));
+        Pane pane = null;
+        try {
+            pane = loader.load();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        LoginScreenController loginScreenController = loader.getController();
+        loginScreenController.setMainScreenController(this);
+        setScreen(pane);
+    }
+
+    public void setScreen(Pane pane) {
+        mainStackPane.getChildren().clear();
+        mainStackPane.getChildren().add(pane);
+    }
+}
